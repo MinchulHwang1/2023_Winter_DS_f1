4240892a44679275446ecda81684ee98215902d1
MinchulHwang1 <hmc912@gmail.com>
2023-01-09 23:05:07 -0500

    Start from baby step. check requirement and make is all in main function, and divide as function.

--- f1.cpp
+++ f1.cpp
@@ -5,27 +5,64 @@
 
 #define kDestinationString 30
 #define kDateSting 10
-#pragma warning(disable:4996)f
+#pragma warning(disable:4996)
+#define _CRT_SECURE_NO_WARNINGS
+
 struct FlightInfo {
 	char* destination;
 	char* date;
 };
 
-void fillFlightInfo(char* des, char* dat);
+//void fillFlightInfo(char* des, char* dat);
 int main() {
-	FlightInfo flightInformation[10] = { };
+	
+	char destinationString[kDestinationString] = "";
+	char dateString[kDateSting] = "";
+	struct FlightInfo flightInformation[10] = { {destinationString, dateString} };
+	int i = 0, count = 2;
 
-	fillFlightInfo(flightInformation->destination, flightInformation->date);
+	for (i; i < count; i++) {
 
+		//fgets(flightInformation->destination, sizeof flightInformation->destination, stdin);
+		//fgets(dateString, sizeof dateString, stdin);
+		fgets(destinationString, sizeof destinationString, stdin);
+		int maxInputDestination = strlen(destinationString);
+		strcpy(flightInformation[i].destination, destinationString);
+		
+	}
+	
+	
+	for (i = 0; i < count; i++) {
+		printf("%s", flightInformation[i].destination);
+	}
+
+	/*
+	int maxInputDestination = strlen(destinationString) ;
+	int maxInputDate = strlen(dateString);
+	
+	//char* mallocDestinationString = (char*)malloc(maxInputDestination);
+	
+	if (mallocDestinationString == NULL) {
+		printf("fawefsdaarser");
+	}
+	else {
+		
+		printf("You entered %s\n", destinationString);
+		printf("%d\n", maxInputDestination);
+		
+		// I don't need the string any more so free it
+		free(mallocDestinationString);
+	}
+	printf("%d\n", maxInputDestination);
+	//fillFlightInfo(flightInformation->destination, flightInformation->date);
+	*/
 
 	return 0;
 }
 
+/*
 void fillFlightInfo(char* des, char* dat) {
-	char destinationString[kDestinationString] = "";
-	char dateString[kDateSting] = "";
-	fgets(destinationString, sizeof destinationString, stdin);
-	fgets(dateString, sizeof dateString, stdin);
+	
 
 	int maxInputDestination = atoi(destinationString) + 1;
 	int maxInputDate = atoi(dateString) + 1;
@@ -41,7 +78,7 @@ void fillFlightInfo(char* des, char* dat) {
 		// I don't need the string any more so free it
 		free(mallocDestinationString);
 	}
-	/*
+	
 	char destinationString[kDestinationString] = "";
 	char dateString[kDateSting] = "";
 
@@ -65,5 +102,5 @@ void fillFlightInfo(char* des, char* dat) {
 
 	printf("%d", maxInputDestination);
 	printf("afwsef %s", des);
-	*/
-}
+	
+}*/
