00ecf3bfc2ac2bafabbc6f1156c509297f53df43
MinchulHwang1 <hmc912@gmail.com>
2023-01-09 22:28:27 -0500

    first design until fillFlightInfo function with example. need to revision

--- f1.cpp
+++ f1.cpp
@@ -1,35 +1,69 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<malloc.h>
+#include<string.h>
 
-#include <stdio.h>
-#include <stdlib.h>	// needed for atoi
-#include <malloc.h>	// needed for malloc
+#define kDestinationString 30
+#define kDateSting 10
+#pragma warning(disable:4996)f
+struct FlightInfo {
+	char* destination;
+	char* date;
+};
 
+void fillFlightInfo(char* des, char* dat);
+int main() {
+	FlightInfo flightInformation[10] = { };
 
-int main(void)
-{
-	// find out how long the string is
-	printf("How long is the string you want to enter?\n");
-	char lengthStr[10] = "";
-	fgets(lengthStr, sizeof lengthStr, stdin);
-	// maxInputLength also adds one for the null-termination
-	int maxInputLength = atoi(lengthStr) + 1;
+	fillFlightInfo(flightInformation->destination, flightInformation->date);
 
-	// allocate the memory, casting the return value from void * to char *
-	char* pString = (char*)malloc(maxInputLength);
-	if (pString == NULL)
-	{
-		printf("Out of memory\n");
-		// a return statement here is not needed since the else bypasses everything else
-		// if I didn't have the else, I would need to get out of the function with return
-	}
-	else
-	{
-		// get the string from the user and then print it
-		fgets(pString, maxInputLength, stdin);
-		printf("You entered <%s>\n", pString);
-
-		// I don't need the string any more so free it
-		free(pString);
-	}
 
 	return 0;
+}
+
+void fillFlightInfo(char* des, char* dat) {
+	char destinationString[kDestinationString] = "";
+	char dateString[kDateSting] = "";
+	fgets(destinationString, sizeof destinationString, stdin);
+	fgets(dateString, sizeof dateString, stdin);
+
+	int maxInputDestination = atoi(destinationString) + 1;
+	int maxInputDate = atoi(dateString) + 1;
+	char* mallocDestinationString = (char*)malloc(maxInputDestination);
+
+	if (mallocDestinationString == NULL) {
+		printf("fawefsdaarser");
+	}
+	else {
+		fgets(mallocDestinationString, maxInputDestination, stdin);
+		printf("You entered %s\n", destinationString);
+
+		// I don't need the string any more so free it
+		free(mallocDestinationString);
+	}
+	/*
+	char destinationString[kDestinationString] = "";
+	char dateString[kDateSting] = "";
+
+	fgets(destinationString, sizeof destinationString, stdin);
+	fgets(dateString, sizeof dateString, stdin);
+	
+	
+
+	int maxInputDestination = atoi(destinationString) + 1;
+	int maxInputDate = atoi(dateString) + 1;
+	printf("flahweklfhakwjlef");
+
+	strncpy(des, destinationString, maxInputDestination);
+	strncpy(dat, dateString, maxInputDate);
+	printf("afwsef %s", des);
+
+	char* mallocDestinationString = (char*)malloc(*des);
+	char* mallocDateString = (char*)malloc(*dat);
+	
+	
+
+	printf("%d", maxInputDestination);
+	printf("afwsef %s", des);
+	*/
 }
